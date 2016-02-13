586a7e1f701364fc3ce02c03c39740982712f789
Sirikon <sirikon2@gmail.com>
2016-02-13 21:07:00 +0100

    Disable temporaly event tracking

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,7 @@ var GenReportLoadingBehaviour = function(){
 		var genReportButton = $('button', genReportForm);
 		genReportForm.addEventListener('submit', function() {
             // track event to Piwik
-            _paq.push(['trackEvent', 'Interactions', 'Generate Report']);
+            //_paq.push(['trackEvent', 'Interactions', 'Generate Report']);
 			genReportButton.setAttribute('disabled', 'true');
 			$('span', genReportButton).textContent = 'Generating report...';
 			$('body').classList.add('wait');
@@ -30,7 +30,7 @@ var LoginWithTwitterLoadingBehaviour = function(){
 		var loginButton = $('button', loginButton);
 		loginButtonContainer.addEventListener('click', function(){
             // track event to Piwik
-            _paq.push(['trackEvent', 'Interactions', 'Login with Twitter']);
+            //_paq.push(['trackEvent', 'Interactions', 'Login with Twitter']);
 			loginButton.setAttribute('disabled', true);
 			loginButton.innerHTML = '<i class="fa fa-spinner fa-spin fa-lg">';
 		});
