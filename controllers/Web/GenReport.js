9597180ef1b10c840f3768b4881838739227bdbf
Sirikon <sirikon2@gmail.com>
2016-02-14 12:57:27 +0100

    Transform GenReport file in a promise chain

--- controllers/Web/GenReport.js
+++ controllers/Web/GenReport.js
@@ -5,36 +5,41 @@ const TwitterProfileRepository = require('../../repositories/TwitterProfileRepos
 const ReportRepository = require('../../repositories/ReportRepository.js');
 const Utils = require('../../utils');
 
-var GenReport = function(req, res, next){
+var GenReport = function(req, res, next) {
 
-	var handleError = (error) => {
+    var handleError = (error) => {
         req.error = error;
         next();
     };
     
-	var lastFollowersIds = req.user.lastFollowers;
-	TwitterRepository.getUserFollowersIds(req.user).then((followersIds) => {
-        var diff = Utils.diff(lastFollowersIds, followersIds);
-		TwitterRepository.lookupUserIds(req.user, [].concat(diff.added).concat(diff.removed))
-			.then((profiles) => {
-                var newReportPromise = null;
-                if (diff.added.length || diff.removed.length) {
-                    newReportPromise = ReportRepository.createReport(req.user, diff);
-                }
-				var profilePromises = profiles.map((profile) => {
-					return TwitterProfileRepository.addOrUpdate(profile);
-				});
-				var remainingPromises = profilePromises;
-				newReportPromise && remainingPromises.push(newReportPromise)
-				Promise.all(remainingPromises)
-					.then(() => {
-						req.user.lastFollowers = followersIds;
-						req.user.save();
-						res.redirect('/home');
-					}, handleError);
-			}, handleError);
-	}, handleError);
-	
+    var lastFollowersIds = req.user.lastFollowers;
+    var diff = null;
+    var followersIds = null;
+    
+    TwitterRepository.getUserFollowersIds(req.user)
+        .then((_followersIds) => {
+            followersIds = _followersIds;
+            diff = Utils.diff(lastFollowersIds, followersIds);
+            return TwitterRepository.lookupUserIds(req.user, [].concat(diff.added).concat(diff.removed))
+        }, handleError)
+        .then((profiles) => {
+            var newReportPromise = null;
+            if (diff.added.length || diff.removed.length) {
+                newReportPromise = ReportRepository.createReport(req.user, diff);
+            }
+            var profilePromises = profiles.map((profile) => {
+                return TwitterProfileRepository.addOrUpdate(profile);
+            });
+            var remainingPromises = profilePromises;
+            newReportPromise && remainingPromises.push(newReportPromise)
+            return Promise.all(remainingPromises)
+        }, handleError)
+        .then(() => {
+            req.user.lastFollowers = followersIds;
+            req.user.save();
+            res.redirect('/home');
+        }, handleError);
+        
 };
 
 module.exports = GenReport;
