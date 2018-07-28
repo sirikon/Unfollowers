60722982696204c6ad8b3c574e8d2542d46a0c87
Carlos Fernández <sirikon2@gmail.com>
2018-07-28 20:47:48 +0200

    fix 2

--- helpers/twitter_picture.js
+++ helpers/twitter_picture.js
@@ -3,5 +3,5 @@
 const Handlebars = require('handlebars');
 
 Handlebars.registerHelper('twitter_picture', function(screen_name, size) {
-	return `https://twitter.com/${screen_name}/profile_image?size=${size}`;
+	return `https://avatars.io/twitter/${screen_name}/medium`;
 });
